5125bb76879a2000c6d2b7c67f225d936ae341ab
Cristian Toro <soporte@engintel.cl>
2022-03-19 21:51:45 -0300

    Test JUnit

--- src/test/java/com/devops/dxc/devops/DevopsApplicationTests.java
+++ src/test/java/com/devops/dxc/devops/DevopsApplicationTests.java
@@ -7,6 +7,7 @@ import com.devops.dxc.devops.model.Util;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+<<<<<<< Updated upstream
 @SpringBootTest
 class DevopsApplicationTests {
 
@@ -14,6 +15,45 @@ class DevopsApplicationTests {
 	@Test
 	void testDxC(){
 		assertEquals(4754104, Util.getDxc(55000000, 15000000));
+=======
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+
+// Segemento para pruebas de métodos utilizados en la aplicación
+//
+@SpringBootTest
+class DevopsApplicationTests {
+
+    @Test
+	@DisplayName("Test Método 10%")
+	void testDxC(){
+		assertEquals(4753645, Util.getDxc(55000000, 1500000));
+		}
+
+
+	@Test
+	@DisplayName("Test Método Impuesto")
+	void testImpuesto(){
+		assertEquals(475364, Util.getImpuesto(55000000, 1500000));
+		}
+	
+	@Test
+	@DisplayName("Test Método Saldo")
+	void testSaldo(){
+		assertEquals(50246355, Util.getSaldo(55000000, 1500000));
+	}
+
+	@Test
+	@DisplayName("Test Api UF")
+	void testUf(){
+		assertEquals(31690.97, Util.getUf());
+>>>>>>> Stashed changes
 	}
 }
 
